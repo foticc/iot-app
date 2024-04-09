be168712e66a3ef4086335b318566f0434bfe88b
foticc <foticc@163.com>
2024-04-09 21:51:24 +0800

    // maven repos

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,8 @@
 pluginManagement {
     repositories {
+        maven { setUrl("https://maven.aliyun.com/repository/public/") }
+        maven { setUrl("https://maven.aliyun.com/repository/central") }
+        maven { setUrl("https://maven.aliyun.com/repository/gradle-plugin") }
         google()
         mavenCentral()
         gradlePluginPortal()
