6f36c544ff112b4f2f1ef4cbf3b289198685663c
foticc <foticc@163.com>
2024-04-09 23:24:59 +0800

    // maven repos

--- app/src/main/java/com/foticc/iot/components/Slider.kt
+++ app/src/main/java/com/foticc/iot/components/Slider.kt
@@ -1,24 +1,34 @@
 package com.foticc.iot.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SliderColors
 import androidx.compose.material3.SliderDefaults
+import androidx.compose.material3.SliderPositions
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.lerp
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PointMode
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 
 @Preview
@@ -41,23 +51,91 @@ fun CustomSliderDemo() {
     var sliderValue by remember {
         mutableFloatStateOf(30f)
     }
-    Slider(
+    Text(text = "${sliderValue}")
 
+
+    Slider(
         value = sliderValue,
         valueRange = 10f..100f,
         onValueChange = {
             sliderValue = it
         },
-        colors = SliderDefaults.colors(
-            thumbColor= Color.Green,
-            activeTickColor = Color.Yellow,
-            activeTrackColor = Color.Green,
-            inactiveTrackColor = Color.Red
-        ),
+//        colors = SliderDefaults.colors(
+//            thumbColor= Color.Green,
+//            activeTickColor = Color.Yellow,
+//            activeTrackColor = Color.Green,
+//            inactiveTrackColor = Color.Red
+//        ),
         thumb = {
-            Icon(imageVector = Icons.Default.FavoriteBorder, contentDescription = "edit")
+//            Icon(imageVector = Icons.Default.FavoriteBorder, contentDescription = "edit")
+        },
+        track = {
+            CustomTrack(sliderPositions = it)
         }
+
 
     )
 }
+
+@Composable
+fun CustomTrack(sliderPositions: SliderPositions,
+                modifier: Modifier = Modifier,
+                colors: SliderColors = SliderDefaults.colors(),
+                enabled: Boolean = true) {
+    Canvas(
+        modifier
+            .fillMaxWidth()
+            .height(20.dp)
+            .clip(RoundedCornerShape(5.dp))
+    ) {
+        val isRtl = layoutDirection == LayoutDirection.Rtl
+        val sliderLeft = Offset(0f, center.y)
+        val sliderRight = Offset(size.width, center.y)
+        val sliderStart = if (isRtl) sliderRight else sliderLeft
+        val sliderEnd = if (isRtl) sliderLeft else sliderRight
+        val tickSize = 1.dp.toPx()
+        val trackStrokeWidth = 20.dp.toPx()
+        drawLine(
+            Color.White,
+            sliderStart,
+            sliderEnd,
+            trackStrokeWidth,
+            StrokeCap.Butt
+        )
+        val sliderValueEnd = Offset(
+            sliderStart.x +
+                    (sliderEnd.x - sliderStart.x) * sliderPositions.activeRange.endInclusive,
+            center.y
+        )
+
+        val sliderValueStart = Offset(
+            sliderStart.x +
+                    (sliderEnd.x - sliderStart.x) * sliderPositions.activeRange.start,
+            center.y
+        )
+
+        drawLine(
+            color = Color(0xff2AB381),
+            sliderValueStart,
+            sliderValueEnd,
+            trackStrokeWidth,
+        )
+        sliderPositions.tickFractions.groupBy {
+            it > sliderPositions.activeRange.endInclusive ||
+                    it < sliderPositions.activeRange.start
+        }.forEach { (outsideFraction, list) ->
+            drawPoints(
+                list.map {
+                    Offset(lerp(sliderStart, sliderEnd, it).x, center.y)
+                },
+                PointMode.Points,
+//                (if (outsideFraction) inactiveTickColor else activeTickColor).value,
+                Color.Yellow,
+                tickSize,
+                StrokeCap.Round
+            )
+        }
+    }
+}
+
 
